6873c7d7f0d5efc2d54f7ce502d950f878acab9e
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-05-21 12:40:33 +0200

    Lesson 3 - Part 1, impl. plus tests

---
+++ Lesson3/test_frog_jump.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from .frog_jump import solution
+
+
+class TestSolution(TestCase):
+    def test_solution(self):
+        self.assertEqual(3, solution(10, 85, 30))
+
+
+
+
+
+
+
+
